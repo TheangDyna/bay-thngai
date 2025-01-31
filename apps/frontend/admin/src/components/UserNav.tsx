9fcd968533cd2feb66a6a1591670192e8f7a3b00
theang-dyna <theangdyna365@gmail.com>
2025-01-31 15:07:40 +0700

    update user nav

--- apps/frontend/admin/src/components/UserNav.tsx
+++ apps/frontend/admin/src/components/UserNav.tsx
@@ -41,17 +41,21 @@ export const UserNav: React.FC = () => {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src="" alt="@profile" />
+            <AvatarFallback>
+              {user.data.email.slice(0, 2).toUpperCase()}
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">
+              {user.data.email.split("@")[0]}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              {user.email}
+              {user.data.email}
             </p>
           </div>
         </DropdownMenuLabel>

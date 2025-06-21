32f07889bf60fb655f614edc161e066f0b42ff61
theang-dyna <theangdyna365@gmail.com>
2025-06-22 00:53:30 +0700

    deploy infra

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-ap-southeast-2-390402568377"
-    key            = "baythngai/dev/terraform.tfstate"
+    key            = "baythngai/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
--- terraform/bootstrap/bootstrap.tf
+++ terraform/bootstrap/bootstrap.tf
@@ -1,18 +1,22 @@
-# s3.tf
+# bootstrap.tf
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket-ap-southeast-2-390402568377" # Must match backend config
+  bucket = "terraform-state-bucket-ap-southeast-2-390402568377"
 
   tags = {
-    Name        = "${var.project_name}-terraform-state"
-    Project     = var.project_name
-    Environment = var.environment
+    Name        = "baythngai-terraform-state"
+    Project     = "baythngai"
+    Environment = "prod"
   }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
-    status = "Enabled" # Required for state consistency
+    status = "Enabled"
   }
 }
 
@@ -20,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
   bucket = aws_s3_bucket.terraform_state.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256" # Enable encryption
+      sse_algorithm = "AES256"
     }
   }
 }
@@ -31,4 +35,19 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    Name        = "baythngai-terraform-locks"
+    Project     = "baythngai"
+    Environment = "prod"
+  }
 }

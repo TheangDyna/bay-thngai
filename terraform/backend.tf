bc8416193ea618a86780739cef6a4c6c2a6dc724
theang-dyna <theangdyna365@gmail.com>
2025-06-21 23:02:14 +0700

    deploy infra

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-ap-southeast-2-390402568377"
-    key            = "baythngai/${terraform.workspace}/terraform.tfstate"
+    key            = "baythngai/dev/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true

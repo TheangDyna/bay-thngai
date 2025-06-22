b478c3d2d7805467079a161fd4b77b02bd59255a
theang-dyna <theangdyna365@gmail.com>
2025-06-22 16:27:17 +0700

    deploy infra

--- terraform/core_api.tf
+++ terraform/core_api.tf
@@ -39,36 +39,6 @@ resource "aws_instance" "app" {
   }
 }
 
-# --- Security Group for EC2 ---
-resource "aws_security_group" "ec2" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 4000
-    to_port     = 4000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow CloudFront (restrict to CloudFront IPs in production)
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict SSH to your IP
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-ec2-sg"
-  }
-}
-
 # --- CloudFront Distribution: Core-API ---
 resource "aws_cloudfront_distribution" "api" {
   origin {
